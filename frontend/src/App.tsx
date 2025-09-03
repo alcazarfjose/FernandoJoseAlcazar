374ef24f5baf27f373935fa295ca89ae051b7aa9
alcazarfjose <alcazarfjose@gmail.com>
2025-09-03 16:07:38 -0700

    feat(gameCard): grid cards with name, details, and animated thumbnails

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import GameCard from "./components/GameCard";
 import TextPrompt from "./components/TextPrompt";
 
@@ -9,21 +9,36 @@ function App() {
   const [intro04, setIntro04] = useState(false); // for ideas i think are cool
   const [intro05, setIntro05] = useState(false); // here are some of them.
 
-  return (
-    <div className="h-screen w-screen relative">
-      <TextPrompt label="hello. " setShow={setIntro01} rules="absolute top-31 left-40 w-full text-black text-5xl font-semibold" />
-      {intro01 && <TextPrompt label="my name is fern. " setShow={setIntro02} rules="absolute top-45 left-60 w-full text-black text-5xl font-semibold"/>}
-      {intro02 && <TextPrompt label="i make small games. " setShow={setIntro03} rules="absolute top-60 left-200 w-full text-black text-5xl font-semibold"/>}
-      {intro03 && <TextPrompt label="for ideas i think are cool. " setShow={setIntro04} rules="absolute top-80 left-180 w-full text-black text-5xl font-semibold" />}
-      {intro04 && <TextPrompt label="here are some of them. " setShow={setIntro05} rules="absolute top-130 left-150 w-full text-black text-5xl font-semibold" />}
+  const gridRef = useRef<HTMLDivElement | null>(null);
 
-      { intro05 && <div className="absolute top-200 grid grid-cols-3 grid-rows-4 gap-15 p-4">
-        <GameCard />
-        <GameCard />
-        <GameCard />
-        <GameCard />
-        <GameCard />
-        <GameCard />
+  // 👇 watch intro05 — when it flips true, scroll into view
+  useEffect(() => {
+    if (intro05 && gridRef.current) {
+      gridRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [intro05]);
+
+  return (
+    <div className="h-screen w-screen relative overflow-x-hidden">
+      <TextPrompt label="hello. " setShow={setIntro01} rules="absolute top-15 left-40 w-full text-black text-5xl font-semibold" />
+      {intro01 && <TextPrompt label="my name is fern. " setShow={setIntro02} rules="absolute top-25 left-60 w-full text-black text-5xl font-semibold"/>}
+      {intro02 && <TextPrompt label="i make small games. " setShow={setIntro03} rules="absolute top-40 left-200 w-full text-black text-5xl font-semibold"/>}
+      {intro03 && <TextPrompt label="for ideas i think are cool. " setShow={setIntro04} rules="absolute top-50 left-180 w-full text-black text-5xl font-semibold" />}
+      {intro04 && <TextPrompt label="here are some of them. " setShow={setIntro05} rules="absolute top-70 left-150 w-full text-black text-5xl font-semibold" />}
+
+      { intro05 && <div className="absolute top-full grid grid-cols-3 grid-rows-4 w-screen" ref={gridRef}>
+        <GameCard
+          name="what do you want to do tonight?"
+          date="Sept 3, 2025"
+          image="-"
+          bullets={["A two-player microgame social game set in rural California", "Ideate and quick-prototype diverse gesture-driven actions", "Build assets and tools for retro PS1-era aesthetic", "Develop multiplayer interface using Unity Netcode API"]}
+        />
+        <GameCard
+          name="what do you want to do tonight?"
+          date="Sept 3, 2025"
+          image="-"
+          bullets={["A two-player microgame social game set in rural California", "Ideate and quick-prototype diverse gesture-driven actions", "Build assets and tools for retro PS1-era aesthetic", "Develop multiplayer interface using Unity Netcode API"]}
+        />
       </div> }
     </div>
   )
--- frontend/src/components/GameCard.tsx
+++ frontend/src/components/GameCard.tsx
@@ -1,18 +1,49 @@
 import React, { useState } from "react";
 
-const GameCard: React.FC = () => {
+type GameCardProps = {
+  name: string;
+  date: string;
+  image: string; // URL or path to the image
+  bullets: string[]; // what type should i put here?
+  link: string;
+};
+
+const GameCard: React.FC = ({ name, date, bullets, image }) => {
   const [isTop, setIsTop] = useState(false);
 
+  // w-128 h-144
   return (
-    <div className="row-span-2 w-128 h-144 bg-blue relative overflow-hidden">
+    <div className="row-span-2 w-full h-144 relative overflow-hidden content-center bg-white">
       <button
         onClick={() => setIsTop(!isTop)}
-        className={`absolute w-full h-1/2 bg-blue-500 text-white cursor-pointer
-          transition-all duration-700 ease-[cubic-bezier(0.68,-0.55,0.27,1.55)]
+        className={`z-10 absolute w-full h-1/2 bg-blue-500 text-white cursor-pointer
+          transition-all duration-450 ease-[cubic-bezier(0.68,-0.55,0.27,1.55)]
           ${isTop ? "top-0" : "top-1/2"}`}
       >
         Click
       </button>
+      <div className={`
+          absolute bottom-0 h-1/2 bg-red-400
+          w-[200%]  /* wider than parent */
+          transition-transform duration-700 ease-in-out
+          [clip-path:polygon(0_0,100%_0,100%_calc(100%-400px),calc(100%-400px)_100%,0_100%)]
+          ${isTop ? "-translate-x-80" : "translate-x-0"}
+        `}></div>
+      <div
+        className={`content-center transition-all duration-1000 ${
+          isTop ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        <h1 className={`absolute font-bold top-1/2 text-4xl text-center italic whitespace-nowrap`}>
+          {name}
+        </h1>
+        <ul className="absolute pl-4 list-disc list-inside bottom-1/4 text-m">
+        {bullets.map((desc, index) => (
+          <li key={index}>{desc}</li>
+        ))}
+        </ul>
+
+      </div>
     </div>
   );
 };
